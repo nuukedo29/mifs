7ab24dea37aff19cf26a94ed8c2360419a308e27
user <>
2020-02-25 12:48:52 +0000

    Innitial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# MIFS - Make it fucking smaller 
+When you cant be fucked to run ffmpeg and 7zip  every time you want to share something on the internet.
+
+Currently only discord (8MB) supported and the script doesn't have alot of logic.
+
+
+
+
+![preview](./doc/preview.png)
+
+## A. Use it like a retard
+`py mifs.py ./path/to/file.ext`
+
+## B. Install it in the context menu
+
+Cant be fucked to write .reg files, so just use [Ultimate Windows Context Menu Customizer](http://www.door2windows.com/ultimate-windows-context-menu-customizer-customize-context-menu-in-windows-xp-vista-7/)
+
+`py G:\_sync\mifs\mifs.py "%1"`
+
+![howto_1](./doc/howto_1.png)
+
+
+#### P.S.
+
+Don't pay furries $4.99/$9.99 a month.
---
+++ mifs.py
@@ -0,0 +1,63 @@
+import sys
+import os 
+import os.path
+import shutil 
+import time
+import traceback
+
+transliterate = False
+
+EXTENSIONS_AUDIO = ["mp3", "wav", "flac", "m4a"]
+EXTENSIONS_VIDEO = ["h264", "mp4", "mov", "avi", "webm", "mkv"]
+EXTENSIONS_IMAGE = ["jpg", "jpeg", "png", "webp", "gif"]
+MAX_SIZE_DISCORD = 1024*1024*8
+
+def number_to_string(number):
+	string = "{:.40f}".format(size).rstrip("0")
+	return string[:-1] if string[-1] == "." else string
+
+if __name__ == "__main__":
+
+	try:	
+
+		file = sys.argv[1]
+
+		filename, extension = os.path.splitext(os.path.basename(file))
+		extension = extension.replace(".", "")
+		folder = os.path.dirname(file) or "."
+		size = os.stat(file).st_size
+
+		output_without_extension = f'{folder}/{filename}_{number_to_string(size)}_mifs'
+		output = f'{output_without_extension}.{extension}'
+
+		if size < MAX_SIZE_DISCORD:
+			shutil.copy(file, output)
+			os._exit(0)
+
+		# TODO: Extract length and calculate best bitrate/resolution possible within target size
+		# TODO: Properly escape windows paths
+
+		if extension in EXTENSIONS_AUDIO:
+			output = f'{output_without_extension}.mp3'
+			if os.path.exists(output): os._exit(0)
+			os.system(f'ffmpeg -loglevel error -i \"{file}\" -ab 128k \"{output}\"')
+
+		elif extension in EXTENSIONS_VIDEO:
+			output = f'{output_without_extension}.mp4'
+			if os.path.exists(output): os._exit(0)
+			os.system(f'ffmpeg -loglevel error -i \"{file}\" -ab 128k -maxrate 2M -bufsize 1M -vf "scale=480:-2" \"{output}\"')
+
+		elif extension in EXTENSIONS_IMAGE:
+			output = f'{output_without_extension}.png'
+			if os.path.exists(output): os._exit(0)
+			os.system(f'ffmpeg -loglevel error -i \"{file}\" -vf "scale=480:-2" \"{output}\"')
+
+		else:
+			output = f'{output_without_extension}.7z'
+			if os.path.exists(output) or os.path.exists(f'{output}.001'): os._exit(0)
+			os.system(f'7z a -v{MAX_SIZE_DISCORD-1024} \"{output_without_extension}.7z\" \"{file}\"')
+
+	except:
+		print(":(\nOOPSIE WOOPSIE!!\n\nOOPSIE WOOPSIE!! Uwu We make a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!\n")
+		traceback.print_exc()
+		time.sleep(3)
